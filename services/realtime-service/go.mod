8fa2c165be9c5bc92c51e22308246b9320571864
Kanishk <vchaganti2@horizon.csueastbay.edu>
2026-02-04 17:29:06 -0800

    Messaging-service → published to Kafka
    Kafka → delivered to realtime-service consumer
    
    Realtime-service → broadcast to all WebSocket clients
    
    WebSocket client → displayed the message

---
+++ services/realtime-service/go.mod
@@ -0,0 +1,11 @@
+module realtime-service
+
+go 1.25.6
+
+require (
+	github.com/go-chi/chi/v5 v5.2.4 // indirect
+	github.com/gorilla/websocket v1.5.3 // indirect
+	github.com/klauspost/compress v1.15.9 // indirect
+	github.com/pierrec/lz4/v4 v4.1.15 // indirect
+	github.com/segmentio/kafka-go v0.4.50 // indirect
+)
---
+++ services/realtime-service/main.go
@@ -0,0 +1,102 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/gorilla/websocket"
+	"github.com/segmentio/kafka-go"
+)
+
+var upgrader = websocket.Upgrader{
+	CheckOrigin: func(r *http.Request) bool { return true },
+}
+
+type Client struct {
+	conn *websocket.Conn
+	send chan []byte
+}
+
+var clients = make(map[*Client]bool)
+var broadcast = make(chan []byte)
+
+func main() {
+	go startKafkaConsumer()
+	go handleBroadcasts()
+
+	http.HandleFunc("/ws", handleWebSocket)
+
+	log.Println("Realtime Service running on :8083")
+	if err := http.ListenAndServe(":8083", nil); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func handleWebSocket(w http.ResponseWriter, r *http.Request) {
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println("WebSocket upgrade error:", err)
+		return
+	}
+
+	client := &Client{
+		conn: conn,
+		send: make(chan []byte),
+	}
+
+	clients[client] = true
+
+	go writePump(client)
+}
+
+func writePump(c *Client) {
+	for msg := range c.send {
+		err := c.conn.WriteMessage(websocket.TextMessage, msg)
+		if err != nil {
+			log.Println("WebSocket write error:", err)
+			c.conn.Close()
+			delete(clients, c)
+			return
+		}
+	}
+}
+
+func handleBroadcasts() {
+	for msg := range broadcast {
+		for client := range clients {
+			select {
+			case client.send <- msg:
+			default:
+				close(client.send)
+				delete(clients, client)
+			}
+		}
+	}
+}
+
+func startKafkaConsumer() {
+	reader := kafka.NewReader(kafka.ReaderConfig{
+		Brokers:  []string{"host.docker.internal:9092"},
+		Topic:    "team-messages",
+		GroupID:  "realtime-consumers",
+		MinBytes: 1,
+		MaxBytes: 10e6,
+		MaxWait:  1 * time.Second,
+	})
+
+	log.Println("Kafka consumer started...")
+
+	for {
+		msg, err := reader.ReadMessage(context.Background())
+		if err != nil {
+			log.Println("Kafka read error:", err)
+			continue
+		}
+
+		fmt.Printf("Received message: %s\n", string(msg.Value))
+		broadcast <- msg.Value
+	}
+}
